cf9c9a95ad13abfa8b6031d6076066ce9dafdb88
Aaryan Singh <aaryansingh@Mac.lan>
2025-08-17 12:18:18 +0530

    changine review bar lable colour to black

--- app/components/RatingBar.tsx
+++ app/components/RatingBar.tsx
@@ -2,7 +2,7 @@ export function RatingBar(){
     return <div className="w-40">
   <div>
     <dd className="flex items-center mb-2">
-      <span className="text-xs font-medium text-gray-500 dark:text-gray-400 me-2 w-4">5</span>
+      <span className="text-xs font-medium me-2 w-4">5</span>
       <div className="w-full rounded-sm h-1.5 bg-slate-300">
         <div
           className="rounded-sm"
@@ -11,7 +11,7 @@ export function RatingBar(){
       </div>
     </dd>
     <dd className="flex items-center mb-2">
-      <span className="text-xs font-medium text-gray-500 dark:text-gray-400 me-2 w-4">4</span>
+      <span className="text-xs font-medium  me-2 w-4">4</span>
       <div className="w-full rounded-sm h-1.5 bg-slate-300">
         <div
           className="rounded-sm"
@@ -20,7 +20,7 @@ export function RatingBar(){
       </div>
     </dd>
     <dd className="flex items-center mb-2">
-      <span className="text-xs font-medium text-gray-500 dark:text-gray-400 me-2 w-4">3</span>
+      <span className="text-xs font-medium  me-2 w-4">3</span>
       <div className="w-full rounded-sm h-1.5 bg-slate-300">
         <div
           className="rounded-sm"
@@ -29,7 +29,7 @@ export function RatingBar(){
       </div>
     </dd>
     <dd className="flex items-center mb-2">
-      <span className="text-xs font-medium text-gray-500 dark:text-gray-400 me-2 w-4">2</span>
+      <span className="text-xs font-medium  me-2 w-4">2</span>
       <div className="w-full rounded-sm h-1.5 bg-slate-300">
         <div
           className="rounded-sm"
@@ -40,7 +40,7 @@ export function RatingBar(){
   </div>
   <div>
     <dd className="flex items-center">
-      <span className="text-xs font-medium text-gray-500 dark:text-gray-400 me-2 w-4">1</span>
+      <span className="text-xs font-medium me-2 w-4">1</span>
       <div className="w-full rounded-sm h-1.5 bg-slate-300">
         <div
           className="rounded-sm"
